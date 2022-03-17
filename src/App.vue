c5eae276775706c50d3218fd14759fe20c628d26
Anurag30112003 <anuragsharma011011@gmail.com>
2022-03-18 02:56:19 +0530

    feat: Navbar(#2)

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Home from './components/Home.vue'
+import Home from './components/Home.vue';
 </script>
 
 <template>
-<Home />
+<Home/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style >
+  @import url('https://fonts.googleapis.com/css2?family=Courier+Prime&family=Maven+Pro&family=Mulish:wght@300&family=Open+Sans&family=Roboto:wght@300&display=swap');
+:root{
+  --primary:#BEE2F1;
+  --secondary:rgb(71, 85, 105);
+  --third:rgb(203, 213, 225);
+  --white:rgb(255, 255, 255);
 }
+
 </style>
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul>
+    <li class="start"><a href="/">anuragdev</a></li>
+    <li><a href="/" class="end">contact</a></li>
+    <li><a href="/">projects</a></li>
+    <li><a class="link" href="/">skills</a></li>
+    <li><a href="/">about</a></li>
+  </ul>
+</template>
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+li {
+  float: right;
+}
+.start {
+  float: left;
+  margin-left: 3vw;
+  
+}
+.end {
+  margin-right: 4vw;
+}
+
+li a {
+  display: block;
+  color: var(--secondary);
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-family: "Mulish", sans-serif;
+  font-size: 1.4rem;
+  font-weight: 600;
+}
+a:hover{
+  text-shadow: 0px 2px 10px rgb(0, 183, 255);                              
+
+}
+</style>
